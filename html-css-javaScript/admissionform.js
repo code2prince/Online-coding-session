a00d6cc279ea50dbf22cf4c12a294c087efced21
github_username <princekumarsharma365@gmail.com>
2022-05-26 16:16:22 +0530

    applied javascript in all pages and modifying on form page & login as well as contact page

--- html-css-javaScript/admissionform.js
+++ html-css-javaScript/admissionform.js
@@ -32,11 +32,11 @@
                 }
             }
 
-            console.log(selectedGender);
+            
 
 
 
-            var radioButtons = document.querySelectorAll('input[name="Occupation"]');
+           var radioButtons = document.querySelectorAll('input[name="Occupation"]');
             var selectedOccupation;
             // for(var i=0; i<radioButtons.length; i++)
             for (var rd of radioButtons) {
@@ -46,14 +46,32 @@
                 }
             }
 
-            console.log(selectedOccupation);
+            var radioButtons = document.querySelectorAll('input[name="qualification2"]');
+            var selectedqualification2;
+            // for(var i=0; i<radioButtons.length; i++)
+            for (var rd of radioButtons) {
+                if (rd.checked) {
+                    selectedqualification2 = rd.value;
+                    break;
+                }
+            }
+            var radioButtons = document.querySelectorAll('input[name="qualification1"]');
+            var selectedqualification1;
+            for (var rd of radioButtons) {
+                if (rd.checked) {
+                    selectedqualification1 = rd.value;
+                    break;
+                }
+            }
+
+           
 
 
            
 
-        //     if(name && email && number  ) {
-        //     console.log('send data to server' ,name,email,number,fathername,mothername,address,qualification,language);
-        //     } else {
-        //     alert('Please provide full detail')
-        //    }
+            if(name && email && number  ) {
+            console.log('send data to server' ,name,selectedGender,qualification,language,email,number,fathername,selectedqualification2,selectedOccupation,mothername,selectedqualification1,address);
+            } else {
+            alert('Please provide full detail')
+           }
         }
