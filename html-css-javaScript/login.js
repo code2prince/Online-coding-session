4248667ad2b701b892bf7df7d5e1fcc9b9a5c235
Rajeev Sharma <talk2rajeev1986@gmail.com>
2022-05-26 17:32:26 +0530

    password validation logic

--- html-css-javaScript/login.js
+++ html-css-javaScript/login.js
@@ -9,3 +9,20 @@ function submitForm(){
         }
     
 }
+
+function validatePassword() {
+    var password = 'a@123'; //document.getElementById('password').value;
+    var format = /[0-9`@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    var isPasswordValid = format.test(password);
+    var length = password.length;
+    if(isPasswordValid) {
+        console.log('password is in valid format'); 
+    } else {
+        console.log('password is not in valid format, use atleaset one number and one special character'); 
+    }
+    if(length >=8 && length <=16){
+        console.log('password is strong length'); 
+    } else {
+        console.log('password must be of length between 8 to 16'); 
+    }
+}
