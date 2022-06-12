03641059b3d292769e3ff13747874d2ef6169ff6
Rajeev Sharma <talk2rajeev1986@gmail.com>
2022-06-12 10:38:30 +0530

    test2: add feature using array methods

---
+++ problem-solving/test2.js
@@ -0,0 +1,136 @@
+const playerList = [
+    {
+        batsmenNo: 1,
+        name: 'Sachin',
+        runs: 105,
+        single: 23,
+        double: 9,
+        three: 2,
+        four: 13,
+        six: 1,
+    },
+    {
+        batsmenNo: 2,
+        name: 'Sehwag',
+        runs: 40,
+        single: 4,
+        double: 1,
+        three: 0,
+        four: 7,
+        six: 1,
+    },
+    {
+        batsmenNo: 3,
+        name: 'Gambir',
+        runs: 15,
+        single: 5,
+        double: 1,
+        three: 0,
+        four: 2,
+        six: 0,
+    },
+    {
+        batsmenNo: 4,
+        name: 'Kohli',
+        runs: 28,
+        single: 10,
+        double: 2,
+        three: 0,
+        four: 3,
+        six: 0,
+    },
+    {
+        batsmenNo: 5,
+        name: 'Rohit',
+        runs: 35,
+        single: 5,
+        double: 4,
+        three: 0,
+        four: 4,
+        six: 1,
+    },
+    {
+        batsmenNo: 6,
+        name: 'Yuvraj',
+        runs: 44,
+        single: 12,
+        double: 2,
+        three: 0,
+        four: 3,
+        six: 3,
+    },
+    {
+        batsmenNo: 6,
+        name: 'Dhoni',
+        runs: 18,
+        single: 2,
+        double: 0,
+        three: 0,
+        four: 4,
+        six: 0,
+    },
+    {
+        batsmenNo: 7,
+        name: 'Zadeja',
+        runs: 0,
+        single: 0,
+        double: 0,
+        three: 0,
+        four: 0,
+        six: 0,
+    },
+    {
+        batsmenNo: 8,
+        name: 'Zaheer',
+        runs: 0,
+        single: 0,
+        double: 0,
+        three: 0,
+        four: 0,
+        six: 0,
+    },
+    {
+        batsmenNo: 9,
+        name: 'Nehra',
+        runs: 0,
+        single: 0,
+        double: 0,
+        three: 0,
+        four: 0,
+        six: 0,
+    },
+    {
+        batsmenNo: 10,
+        name: 'Harbhajan',
+        runs: 0,
+        single: 0,
+        double: 0,
+        three: 0,
+        four: 0,
+        six: 0,
+    },
+    {
+        batsmenNo: 11,
+        name: 'RP Singh',
+        runs: 0,
+        single: 0,
+        double: 0,
+        three: 0,
+        four: 0,
+        six: 0,
+    },
+];
+
+
+// 1. get the total no of RUNs scored by all batsmen
+
+// 2. get the total no of FOURs hit by all batsmen
+
+// 3. get the total no of SIXs hit by all batsmen
+
+// 4. get the total no of SINGLEs scrored by all batsmen
+
+// 5. get the total no of DOUBLEs scrored by all batsmen
+
+// 6. get the total no of THREEs scrored by all batsmen
+
