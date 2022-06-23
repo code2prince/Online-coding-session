b016bfb38c48794a0d5fb8a7fbb086867e0c86ad
github_username <princekumarsharma365@gmail.com>
2022-06-23 17:40:36 +0530

    highest tower problem

--- problem-solving/highestTowerInCity.js
+++ problem-solving/highestTowerInCity.js
@@ -3,7 +3,18 @@
 var towerArr = [4,21,15,18, 27, 58, 33, 21, 45, 12];
 
 function getHighestTower(towers) {
+    var maxHight=0;
 
+    for(i=0; i<towerArr.length; i++){
+        if(towerArr[i]> maxHight){
+            maxHight=towerArr[i];
+
+
+        }
+    }
+
+
+console.log("max hight of tower:", maxHight);
 }
 
 getHighestTower(towerArr);
