fdcfd11d3ecbac4b9c00f190c1f3730fe18bcc8f
github_username <princekumarsharma365@gmail.com>
2022-06-23 17:39:28 +0530

    bubble sort

--- problem-solving/bubbleSort.js
+++ problem-solving/bubbleSort.js
@@ -2,8 +2,23 @@
 
 var arr = [4,3,6,0,-1, 9, 5, 7, 8];
 
-function factorial(num) {
-
+function bubbleSort(arr) {
+    
+  
+    for(i=0; i<arr.length; i++){
+        
+        for(j=0; j<arr.length; j++){
+            if(arr[j]> arr[j+1]){
+                //swap
+                var temp= arr[j];
+                arr[j]= arr[j+1];
+                arr[j+1]= temp;
+               
+            }
+        }
+        
+    }
+    console.log(arr);
 }
 
-factorial(5);
+bubbleSort(arr);
