b3f626a7400e5d944c6392058c4bf1f0318cb1c8
github_username <princekumarsharma365@gmail.com>
2022-06-24 17:29:17 +0530

    prime no

--- problem-solving/primeNumber.js
+++ problem-solving/primeNumber.js
@@ -1,4 +1,27 @@
-// 1. write a function to find first 5 prime numbers
+// 1. write a function to find prime no b/w  0 to 10
+// prime no [ 2,3,5,7,11]
+function prime(num){
+    if(num===2){
+        return true;
+    }
+    else if(num>1){
+        for(i=2; i<num; i++){
+            if(num % i !==0){
+                return true;
+            }
+            else if(num === i*1){
+                return false;
+            }
+            else{
+                return false;
+            }
+        }
+        
+    }
+}
+console.log(prime(8));
+    
+
 
 
 
