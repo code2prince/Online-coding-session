10bd64872d0e1951b0d9c5702ba7d858d07573bc
Rajeev Sharma <talk2rajeev1986@gmail.com>
2022-06-25 06:24:47 +0530

    login and userRoles

---
+++ problem-solving/userRole.js
@@ -0,0 +1,31 @@
+var users = [
+    {
+        username: 'abc@gmail.com',
+        password: '12345',
+        roles: ["admin"],
+    },
+    {
+        username: 'xyx@gmail.com',
+        password: '54321',
+        roles: ["view only"],
+    },
+    {
+        username: 'pqr@gmail.com',
+        password: '55441',
+        roles: ["admin", "view only"],
+    },
+    {
+        username: 'mno@gmail.com',
+        password: '44556',
+        roles: ["admin"],
+    },
+    {
+        username: 'klm@gmail.com',
+        password: '33445',
+        roles: ["admin"],
+    },
+];
+
+
+// write a function to get all the users having roles as "admin" & "view only" both
+// write a function to get all the users having roles as "admin" role only
