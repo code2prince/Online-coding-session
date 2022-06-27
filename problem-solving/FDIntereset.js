6cc7ceda184bb2dc3b91ef9fdbe486a7ff191dcd
Rajeev Sharma <talk2rajeev1986@gmail.com>
2022-06-27 20:42:34 +0530

    intereset calculation

---
+++ problem-solving/FDIntereset.js
@@ -0,0 +1,34 @@
+// given list is the bank accounts for Fixed Deposit
+// 1. calculate simple interest on balance amount and add it to the balance amount
+// For example accountNo: '1000001' has Rs 100000 in account, after interest calculation balance amount becomes 100005
+
+
+const interestRate = 5;
+
+const fixedDepositAccounts = [
+    {
+        accountNo: '1000001',
+        balance: '100000',
+    },
+    {
+        accountNo: '1000002',
+        balance: '87501',
+    },
+    {
+        accountNo: '1000003',
+        balance: '250000',
+    },
+    {
+        accountNo: '1000004',
+        balance: '65900',
+    },
+    {
+        accountNo: '1000005',
+        balance: '125000',
+    },
+    {
+        accountNo: '1000006',
+        balance: '210000',
+    }
+];
+
