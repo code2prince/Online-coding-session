acd99c36c9cae71b3b30d8752bff0ae6a93a92ad
Rajeev Sharma <talk2rajeev1986@gmail.com>
2022-06-28 23:11:29 +0530

    remove vowels from word'sentence

---
+++ problem-solving/removeVowel.js
@@ -0,0 +1,5 @@
+// write a function to remove vowels from given word or sentence.
+
+// Example: "This is Rohit Sharma, who played for U-19 India in 2006";
+// Output:- "Ths s Rht Shrm, wh plyd fr -19 nd n 2006"
+
