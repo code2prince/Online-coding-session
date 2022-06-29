850059aa8a1eb7b5faac3036a76784cb3edc58a2
Rajeev Sharma <talk2rajeev1986@gmail.com>
2022-06-29 10:27:45 +0530

    start pattern print

---
+++ problem-solving/patternPrint.js
@@ -0,0 +1,19 @@
+/* 
+
+write a funtion to print a pattern as given below
+Output:-
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+
+*/
+
+// as you can see, 1st line has one star, 2nd line has 2 star, 
+// 5th line has 5 star then from 6th line star are decreasing one by one till it reaches 1
