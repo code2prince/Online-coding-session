0246017f696bbb7861c7965ffc8e5617e0802ac4
github_username <princekumarsharma365@gmail.com>
2022-06-29 20:48:18 +0530

    vowel removed  problem

--- problem-solving/removeVowel.js
+++ problem-solving/removeVowel.js
@@ -3,3 +3,36 @@
 // Example: "This is Rohit Sharma, who played for U-19 India in 2006";
 // Output:- "Ths s Rht Shrm, wh plyd fr -19 nd n 2006"
 
+
+
+
+function remVowel(string){
+    var vowels = ['a','e','i','o','u'];
+    var a="";
+    for(i=0;i<string.length; i++){
+        if(!vowels.includes(string[i].toLowerCase()))
+        {
+            a+= string[i];
+        }
+    }
+    console.log(a);
+    return a;
+}
+var string= 'I am Prince Sharma';
+
+remVowel(string);
+
+
+// function removeVowel(sentance){
+//     var sentanceArray=sentance.split('');
+//     for(i=0; i<sentance.length; i++){
+//         var vowel= sentance[i];
+//         if(vowel=="a" || vowel== "e" || vowel=="i" || vowel=="o" || vowel== "u"){
+//             sentanceArray[i]='';
+//         }
+//     }
+//     return sentanceArray.join('');
+// }
+// removeVowel('I am Prince sharma');
+
+
