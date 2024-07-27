2069f338e7dcd9bd1bdc0a267e37f3f935b96709
github_username <princekumarsharma365@gmail.com>
2024-07-27 19:30:58 +0530

    callback Hell

--- problem-solving/Js/callBack.js
+++ problem-solving/Js/callBack.js
@@ -11,14 +11,14 @@ function saveToDb(msg, success, failure) {
     }
 }
 
-saveToDb(() => {
-    console.log("Welcome")
-    }, () => {
-        console.log("Success Callback: Internet speed is good ,Inserted")
-    }, () => {
-        console.log("Failure Callback: Weak Internet Connection")
-    }
-)
+// saveToDb(() => {
+//     console.log("Welcome")
+//     }, () => {
+//         console.log("Success Callback: Internet speed is good ,Inserted")
+//     }, () => {
+//         console.log("Failure Callback: Weak Internet Connection")
+//     }
+// )
 
 
 /*
@@ -30,4 +30,46 @@ Failure Callback: Weak Internet Connection
 Welcome
 7
 Success Callback: Internet speed is good ,Inserted
+*/
+
+
+
+// callBack Hell   -->pyramid of CallBacks its also a bad practice to avoid it use promises or Async-Await
+saveToDb(() => {
+    console.log("Welcome")
+}, () => {
+    console.log("Success Callback: Internet speed is good ,Inserted")
+    saveToDb(() => {
+    }, () => {
+        console.log("Success Callback: Internet speed is good ,Inserted")
+    }, () => {
+        console.log("Failure Callback: Weak Internet Connection")
+    }
+    )
+}, () => {
+    console.log("Failure Callback: Weak Internet Connection")
+}
+)
+
+
+//---------------call back hell-------------
+/*
+
+
+Welcome
+4
+Success Callback: Internet speed is good ,Inserted
+7
+Success Callback: Internet speed is good ,Inserted
+ ---------------------
+Welcome
+6
+Success Callback: Internet speed is good ,Inserted
+2
+Failure Callback: Weak Internet Connection
+
+-------------------
+Welcome
+1
+Failure Callback: Weak Internet Connection
 */
